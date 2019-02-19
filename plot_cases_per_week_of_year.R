f87020623e56f3d0c1674cef399b2ded6d181323
Caroline Mimeault <carolinemimeault@gmail.com>
2019-02-18 20:16:27 -0500

    adjust title display

--- plot_cases_per_week_of_year.R
+++ plot_cases_per_week_of_year.R
@@ -5,6 +5,5 @@ cases <- read_csv("dengue_labels_train.csv")
 cases %>% ggplot() + 
   geom_boxplot(aes(weekofyear, total_cases, group=weekofyear)) + 
   facet_grid(city ~ ., scale = "free") + 
-  ggtitle("Total number of cases per week-of-the-year in each city") + 
-  theme(plot.title = element_text(hjust = 1.5, face="bold"))
+  ggtitle("Total number of cases per week-of-the-year in each city") 
 ggsave("cases_per_week.png", device="png")
