484488593c386e982d24067b823565d4467d6d71
Caroline Mimeault <carolinemimeault@gmail.com>
2019-03-14 23:44:40 -0400

    formatting

--- plot_cases_per_week_of_year.R
+++ plot_cases_per_week_of_year.R
@@ -2,8 +2,9 @@ library(tidyverse)
 
 cases <- read_csv("dengue_labels_train.csv")
 
-cases %>% ggplot() + 
-  geom_boxplot(aes(weekofyear, total_cases, group=weekofyear)) + 
-  facet_grid(city ~ ., scale = "free") + 
-  ggtitle("Total number of cases per week-of-the-year in each city") 
-ggsave("cases_per_week_of_year.png", device="png")
+cases %>%
+  ggplot() +
+  geom_boxplot(aes(weekofyear, total_cases, group = weekofyear)) +
+  facet_grid(city ~ ., scale = "free") +
+  ggtitle("Total number of cases per week-of-the-year in each city")
+ggsave("cases_per_week_of_year.png", device = "png")
--- script_to_merge_datasets.R
+++ script_to_merge_datasets.R
@@ -2,7 +2,7 @@
 # https://www.drivendata.org/competitions/44/dengai-predicting-disease-spread/
 
 # clear workspace memory
-remove(list=ls())
+remove(list = ls())
 
 # import libraries
 library(tidyverse)
@@ -11,7 +11,7 @@ cases <- read_csv("dengue_labels_train.csv")
 parameters <- read_csv("dengue_features_train.csv")
 
 # merge the cases and parameters file
-merged_training_data <- merge(cases, parameters, by=c("city","year","weekofyear"))
+merged_training_data <- merge(cases, parameters, by = c("city", "year", "weekofyear"))
 
 # save data as csv file
 write_csv(merged_training_data, path = "merged_training_data.csv")
