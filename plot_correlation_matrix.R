39fe445b53e11618e12aaee5fd9d791c59543c9e
Caroline Mimeault <carolinemimeault@gmail.com>
2019-03-15 20:54:19 -0400

    getting ordered correlation values

--- plot_correlation_matrix.R
+++ plot_correlation_matrix.R
@@ -4,6 +4,15 @@ library(ggcorrplot)
 data <- read_csv("clean_training_data.csv") %>%
   select_if(is.numeric)
 
+# to plot the correlation matrix
 correl <- cor(data, use = "all.obs", method="pearson")
 ggcorrplot(correl)
 ggsave("correlation_matrix.png", device="png")
+
+# to extract the results
+res <- cor(data)
+res_df <- as.data.frame(res)
+res_df <- res_df %>% rownames_to_column('variables')
+res_df_tc <- res_df %>% select(variables, total_cases)
+res_df_tc <- arrange(res_df_tc, total_cases)
+res_df_tc
